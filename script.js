84fee9c669ace2a559d1195e8e6c16482054429e
Popular234 <cainjose24@g.crookcountyschools.org>
2022-11-08 13:27:27 -0800

    added prompt

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("hello world")
+
+alert("here in this game you will make a series of choices to go down different paths, some choices can be fatal and can send you back to the start. When making these choices we ask that you answer in the format that is asked of you and you are using the answers provided to you instead of one that is not.")
