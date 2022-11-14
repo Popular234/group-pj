bff98e510463c9b00e5d6aba27d9abbcead3dcc4
Popular234 <cainjose24@g.crookcountyschools.org>
2022-11-14 13:57:05 -0800

    added a prompt with the first choice

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # group-pj
-
-/* http://meyerweb.com/eric/tools/css/reset/ 
+<!-- 
+   http://meyerweb.com/eric/tools/css/reset/ 
    v2.0 | 20110126
-   License: none (public domain)
-*/
+   License: none (public domain) -->
+
+<!-- https://sebhastian.com/javascript-create-button/ -->
 
    Group Names
 Cain Jose, Sammy Jones
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 console.log("hello world")
 
-alert("here in this game you will make a series of choices to go down different paths, some choices can be fatal and can send you back to the start. When making these choices we ask that you answer in the format that is asked of you and you are using the answers provided to you instead of one that is not.")
+function strtgam(){
+    var firstchoice = prompt("you start in a small cabin deep in the woods, your objective is to discover the 3 pages. You wake up early in the morning to hear scrathing at your wall above your head. you decide to get up and check it out. Before you head out front to check out the noise you have a few options, you can choose to go to the right, out the back of the cabin, straight, to the kitchen to make food, or left, outside to investigate the noise.")
+}
